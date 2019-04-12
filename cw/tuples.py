08271b2b8a02797bcf67edf6e9279cbf4fb61846
artkoc7548 <artur@koczwara.net.pl>
2019-04-12 14:49:38 +0200

    Update tuples.py

--- cw/tuples.py
+++ cw/tuples.py
@@ -1,3 +1,28 @@
+'''
+Przygotowujesz się do analizy email-marketingu. Po każdym zadaniu poniżej wyświetl listę:
+
+1. Utwórz listę o nazwie marketing z elementami:
+
+-loyality program
+-client promotion
+-market research
+
+2. Dodaj do listy element 'public relations'
+
+3. Wyświetl pozycję numer 3
+
+4. Wstaw na pozycję numer 2 element 'investor relations'
+
+5. Chcesz jednak aby lista znajdowała się w zmiennej o nazwie emailMarketing. Skopiuj elementy z listy marketing do listy emailMarketing
+
+6. Posortuj listę emailMarketing
+
+7. Utwórz nową jednoelementową listę internalEmails. Jedyny element to 'internal communication'
+
+8. Dodaj listę internalEmails do listy emailMarketing
+
+9. Utwórz tuple, którego wartości pochodzą z listy emailMarketing. Podczas wyświetlania tuple zwróć uwagę na nawiasy, z jakich skorzystał Python
+'''
 marketing=['loyality program','client promotion','market research']
 print(marketing)
 marketing.append('public relations')
